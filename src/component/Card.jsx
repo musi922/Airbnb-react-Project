dcdc14f63a528fec30704a26eb4d4ddf646beec9
Richard <richardmusime6@gmail.com>
2024-07-22 10:30:41 +0200

    Added the card in the component folder

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { FaStar } from 'react-icons/fa';
 
-export default function Card(props){
+export default function Card({image, }){
+    const hours = new Date().getHours()
     return(
         <div className="pt-[22%] pl-32 pb-12">
-            <img src={props.image} className="rounded-s-md h-[45vh] w-full "  />
+            <img src={image} className="rounded-s-md h-[45vh] w-full "  />
             <div className="rounded-md h-10 w-32 bg-white absolute mt-[-20.7%] ml-6 text-center">
-                <h1 className="p-2">{props.name}</h1>
+                <h1 className="p-2">{(props.name).toUpperCase()}</h1>
             </div>
             <div className="flex gap-1">
              <FaStar className="text-[#FE395C] pt-1"/>
